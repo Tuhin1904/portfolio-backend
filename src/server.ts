8fb1babd0cd28dc214547b5b3e589d538bbb118b
Tuhin1904 <iamtuhinn@gmail.com>
2026-07-11 23:18:22 +0530

    Fixed the error : Error: connect ENETUNREACH

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
+
 import app from "./app";
 import { connectDB } from "./config/db";
 import http from "http";
